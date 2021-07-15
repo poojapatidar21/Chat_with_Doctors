8138cde318dad653d7c6f84a7ebcfcec9a366521
pooja21082001 <poojapatidar897@gmail.com>
2021-07-15 22:23:15 +0530

    add list of specialization

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,13 +3,55 @@ import { Image, View,TouchableOpacity, Modal, FlatList, Text } from "react-nativ
 import { ListItem } from "react-native-elements";
 
 const profilePic  =require("../images/profile-user.png");
-
+const pediatric =require("../images/pediatric.png");
+const dermatologist =require("../images/dermatologist.jpg");
+const diet_advice =require("../images/diet_advice.png");
+const gastronterology =require("../images/gastronterology.png");
+const gynocology =require("../images/gynocology.png");
+const homeopathy =require("../images/homeopathy.jpg");
+const medical =require("../images/medical.jpg");
+const nerological =require("../images/nerological.png");
+const psychiatry =require("../images/psychiatryr.png");
+const sexology =require("../images/sexology.jpg");
 
 let list=[
     {
-        imageUrl:profilePic,
-        name:"xyz",
+        imageUrl:pediatric,
+        name:"pediatric",
+    },
+    {
+        imageUrl:dermatologist,
+        name:"dermatologist",
+    },
+    {
+        imageUrl:diet_advice,
+        name:"diet_advice",
+    },
+    {
+        imageUrl:gastronterology,
+        name:"gastronterology",
+    },
+    {
+        imageUrl:gynocology,
+        name:"gynocology",
+    },
+    {
+        imageUrl:homeopathy,
+        name:"homeopathy",
+    },{
+        imageUrl:medical,
+        name:"medical",
+    },{
+        imageUrl:nerological,
+        name:"nerological",
+    },{
+        imageUrl:psychiatry,
+        name:"psychiatry",
+    },{
+        imageUrl:sexology,
+        name:"sexology",
     }
+    
 ]
 
 export class Categories extends Component{
